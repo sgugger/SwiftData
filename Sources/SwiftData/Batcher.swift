962deeae24e169fba8b9c427a7ddf2222f63d817
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-12-15 14:41:21 -0800

    Update names and tutorial

--- Sources/SwiftData/Batcher.swift
+++ Sources/SwiftData/Batcher.swift
@@ -1,20 +1,21 @@
 import TensorFlow
 
-public protocol BatcherConfig {
+public protocol BatcherTemplate {
     associatedtype Item
     associatedtype Sample
     associatedtype RawBatch
     associatedtype Batch
     
+    func sampleIndices (_ dataset: [Item], shuffled: Bool) -> [Int]
     func processItem   (_ item:    Item)     -> Sample
     func processSamples(_ samples: [Sample]) -> [Sample]
     func collateSamples(_ samples: [Sample]) -> RawBatch 
     func processBatch  (_ batch:   RawBatch) -> Batch
 }
 
-public struct Batcher<C>: Sequence where C: BatcherConfig {
-    public let config: C
-    public let dataset: [C.Item]
+public struct Batcher<T>: Sequence where T: BatcherTemplate {
+    public let template: T
+    public let dataset: [T.Item]
     public var batchSize: Int
     public var numWorkers: Int = 1
     public var shuffle: Bool = false
@@ -24,56 +25,60 @@ public struct Batcher<C>: Sequence where C: BatcherConfig {
         return dataset.count / batchSize + (dataset.count % batchSize == 0 || dropLast ? 0 : 1)
     }
     
-    public init(_ config: C, 
-                dataset: [C.Item], 
+    public init(from template: T, 
+                on dataset: [T.Item], 
                 batchSize: Int, 
                 numWorkers: Int = 1, 
                 shuffle: Bool = false, 
                 dropLast: Bool = false) {
-        (self.config, self.dataset, self.batchSize) = (config, dataset, batchSize)
+        (self.template, self.dataset, self.batchSize) = (template, dataset, batchSize)
         (self.numWorkers, self.shuffle, self.dropLast) = (numWorkers, shuffle, dropLast)
     }
     
-    public func makeIterator() -> BatchIterator<C> {
+    public func makeIterator() -> BatchIterator<T> {
         return BatchIterator(self)
     }
 }
 
-public struct BatchIterator<C>: IteratorProtocol where C: BatcherConfig {
-    let b: Batcher<C>
+public struct BatchIterator<T>: IteratorProtocol where T: BatcherTemplate {
+    let b: Batcher<T>
     let indices: [Int]
     var pos: Int = 0
     
-    init(_ b: Batcher<C>) { 
+    init(_ b: Batcher<T>) { 
         self.b = b
         pos = 0
-        indices = b.shuffle ? Array(0..<b.dataset.count).shuffled() : Array(0..<b.dataset.count)
+        indices = b.template.sampleIndices(b.dataset, shuffled: b.shuffle)
     }
     
-    public mutating func next() -> C.Batch? {
+    public mutating func next() -> T.Batch? {
         guard pos < b.dataset.count else { return nil }
         let end = min(pos + b.batchSize, b.dataset.count)
         if (end - pos) < b.batchSize && b.dropLast { return nil }
         //The idea is to have samples processed and collated on the CPU before moving to the host, not sure this is the right way.
-        let batch = withDevice(.cpu) { () -> C.RawBatch in
+        let batch = withDevice(.cpu) { () -> T.RawBatch in
             let samples = Array(pos..<end).concurrentMap(nthreads: b.numWorkers) {
-                b.config.processItem(b.dataset[indices[$0]])
+                b.template.processItem(b.dataset[indices[$0]])
             }
             pos = end
-            return b.config.collateSamples(b.config.processSamples(samples))
+            return b.template.collateSamples(b.template.processSamples(samples))
         }
-        return b.config.processBatch(batch)
+        return b.template.processBatch(batch)
     }
 }
 
-public extension BatcherConfig where Item == Sample {
+public extension BatcherTemplate where Item == Sample {
     func processItem(_ item: Item) -> Sample { return item }
 }
 
-public extension BatcherConfig {
+public extension BatcherTemplate {
+    func sampleIndices (_ dataset: [Item], shuffled: Bool) -> [Int] {
+        return shuffled ? Array(0..<dataset.count).shuffled() : Array(0..<dataset.count)
+    }
+    
     func processSamples(_ samples: [Sample]) -> [Sample] { return samples }
 }
 
-public extension BatcherConfig where RawBatch == Batch {
+public extension BatcherTemplate where RawBatch == Batch {
     func processBatch(_ batch: RawBatch) -> Batch { return batch }
 }
