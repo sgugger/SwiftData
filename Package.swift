9e2b92f3b238d530cb9a47b30c2bdcb488ce664d
Sylvain Gugger <sylvain.gugger@gmail.com>
2020-01-08 11:41:48 -0800

    Clean up

--- Package.swift
+++ Package.swift
@@ -5,14 +5,12 @@ import PackageDescription
 let package = Package(
     name: "SwiftData",
     products: [
-      .library( name: "SwiftData", targets: ["SwiftData"]),
-      .library( name: "SwiftData2", targets: ["SwiftData2"])
+      .library( name: "Batcher", targets: ["Batcher"])
     ],
     dependencies: [
         .package(url: "https://github.com/mxcl/Path.swift", from: "0.16.3")
     ],
     targets: [
-        .target( name: "SwiftData", dependencies: ["Path"]),
-        .target( name: "SwiftData2", dependencies: ["Path"])
+        .target( name: "Batcher", dependencies: ["Path"])
     ]
 )
