927e749c9182f056bcfa8870528e2e5bd594a963
Dave Abrahams <dabrahams@google.com>
2020-04-02 12:15:29 -0700

    Comment/reface Batches.

--- Epochs/Batches.swift
+++ Epochs/Batches.swift
@@ -1,44 +1,63 @@
-public struct Batches<Batch, BatchSampleSet: Collection> {
+/// A collection of `Batch` whose elements are successive chunks of
+/// a sample set, lazily mapped through a batch creation closure.
+///
+/// In typical use, `Batch` will consist of one (for inference/unsupervised
+/// training) or two (for supervised training) tensors, and the closure stacks
+/// the chunk of samples into a `Batch`, if necessary applying a transformation
+/// first to ensure they are uniform.
+public struct Batches<BatchSampleSet: Collection, Batch> {
   /// The underlying samples.
-  let samples: BatchSampleSet
+  private let samples: BatchSampleSet
   /// The size of every batch, except possibly the last one.
-  let batchSize: Int
+  private let batchSize: Int
   /// Returns a batch given the constituent batch samples.
-  let makeBatch: (BatchSampleSet.SubSequence) -> Batch
-  
-  public init(on samples: BatchSampleSet, batchSize: Int, 
-              makeBatch: @escaping (BatchSampleSet.SubSequence) -> Batch) {
+  private let makeBatch: (BatchSampleSet.SubSequence) -> Batch
+
+  /// Creates an instance that presents successive chunks, of size `batchSize`,
+  /// from `samples`, lazily transformed by `transform`.
+  ///
+  /// If `batchSize % samples.count != 0`, the final chunk presented to
+  /// `transform` will have fewer samples: `(samples.count - 1) % batchSize +
+  /// 1`.
+  public init(
+      of batchSize: Int, from samples: BatchSampleSet, 
+      _ transform: @escaping (BatchSampleSet.SubSequence) -> Batch
+  ) {
     self.samples = samples
     self.batchSize = batchSize
-    self.makeBatch = makeBatch
+    self.makeBatch = transform
   }
 }
 
 extension Batches : Collection {
+  /// A type whose instances represent positions in `self`.
   public typealias Index = BatchSampleSet.Index
-  
+
+  /// The position of the first element.
   public var startIndex: BatchSampleSet.Index {
     return samples.startIndex
   }
 
+  /// The position one past the last element.
   public var endIndex: BatchSampleSet.Index {
     return samples.endIndex
   }
 
+
+  /// Returns the position immediately following `i`.
   public func index(after i: Index) -> Index {
     return samples.index(i, offsetBy: batchSize, limitedBy: endIndex)!
   }
 
+  /// Returns the element at `i`.
   public subscript(i: Index) -> Batch {
     makeBatch(samples[i..<index(after: i)])
   }
 }
 
-public func defaultMakeBatch<BatchSamples: Collection>(samples: BatchSamples)
-    -> BatchSamples.Element
-    where BatchSamples.Element: Collatable
-{
-  return .init(collating: samples)
+extension Collection where Element: Collatable {
+  /// The result of collating the elements of `self`.
+  public var collated: Element { .init(collating: self) }
 }
 
 public struct SelectedElements<Base: Collection, Order: Collection>
