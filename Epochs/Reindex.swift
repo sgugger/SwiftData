2115399a4a31e14ba5576a85997c137b8573cff7
Sylvain Gugger <sylvain.gugger@gmail.com>
2020-04-10 06:09:11 -0700

    Make reindex optional

--- Epochs/Reindex.swift
+++ Epochs/Reindex.swift
@@ -1,19 +1,13 @@
 /// A collection obtained by re-indexing a base collection.
 public struct ReindexedCollection<Base: RandomAccessCollection> {
   /// The new index
-  private var reindex: [Base.Index]
+  private var reindex: [Base.Index]!
   /// The base collection  
   private let base: Base
   
   /// Creates an instance from `base` and `reindex`.
-  public init(base: Base, reindex: [Base.Index]) {
+  public init(_ base: Base, reindex: [Base.Index]! = nil) {
     self.reindex = reindex
-    self.base = base
-  }
-    
-  /// Creates an instance from `base` and its indices.
-  public init(_ base: Base) {
-    self.reindex = Array(base.indices)
     self.base = base
   }
 }
@@ -25,23 +19,21 @@ extension ReindexedCollection: RandomAccessCollection {
   public typealias Index = Int
 
   /// The position of the first element.
-  public var startIndex: Index {
-    return reindex.startIndex
-  }
+  public var startIndex: Int { 0 }
 
   /// The position one past the last element.
-  public var endIndex: Index {
-    return reindex.endIndex
-  }
+  public var endIndex: Int { base.count  }
 
   /// Returns the position immediately following `i`.
-  public func index(after i: Index) -> Index {
-    return reindex.index(after: i)
-  }
+  public func index(after i: Int) -> Index { i+1 }
 
   /// Returns the element at `i`.
   public subscript(i: Index) -> Element {
-    base[reindex[i]]
+    if reindex==nil {
+      return base[base.index(base.startIndex, offsetBy: i)]
+    } else {
+      return base[reindex[i]]
+    }
   }
 }
 
@@ -61,24 +53,29 @@ private func sortIndexInBatches<Index>(
 public extension ReindexedCollection {
   /// Shuffles its `reindex` (thus not breaking the laziness if `base` is lazy
   mutating func innerShuffle() {
+    if reindex == nil { reindex = Array(base.indices) }
     reindex.shuffle()
   }
     
   /// Returns an instance of self with `reindex` shuffled.
   func innerShuffled() -> Self {
-    return ReindexedCollection(base: base, reindex: reindex.shuffled())
+    return ReindexedCollection(
+      base, reindex: (reindex ?? Array(base.indices)).shuffled())
   }
  
   /// Sorts itself in batches
   mutating func sortInBatches(
       of batchSize: Int, by areInOrder: (Base.Index, Base.Index)->Bool
   ){
-    reindex = sortIndexInBatches(of: batchSize, index: reindex, by: areInOrder)
+    reindex = sortIndexInBatches(
+      of: batchSize, index: reindex ?? Array(base.indices), by: areInOrder)
   }
   
   func sortedInBatches(
       of batchSize: Int, by areInOrder: (Base.Index, Base.Index)->Bool
   ) -> Self {
-    return ReindexedCollection(base: base, reindex: sortIndexInBatches(of: batchSize, index: reindex, by: areInOrder))
+    return ReindexedCollection(
+      base, reindex: sortIndexInBatches(
+        of: batchSize, index: reindex ?? Array(base.indices), by: areInOrder))
   }
 }
