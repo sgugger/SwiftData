a265a7f23b1f306eba28cef42b0338bd88d624c3
Dave Abrahams <dabrahams@google.com>
2020-04-13 15:54:12 -0700

    Add dir locals to standardize formatting.

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((swift-mode
+  (swift-mode:parenthesized-expression-offset . 4)
+  (swift-mode:multiline-statement-offset . 4)
+  (swift-mode:basic-offset . 4)))
+
+
