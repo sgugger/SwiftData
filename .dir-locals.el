b3e24a2700c328484efafbca5de7828b288d043e
Dave Abrahams <dabrahams@google.com>
2020-04-13 15:56:12 -0700

    Consistent formatting

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,8 +2,10 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((swift-mode
-  (swift-mode:parenthesized-expression-offset . 4)
   (swift-mode:multiline-statement-offset . 4)
+  (swift-mode:parenthesized-expression-offset . 4)
   (swift-mode:basic-offset . 4)))
+
+
 
 
