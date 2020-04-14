bfac7e8f4d05e47906808336d9b4175dddac2c07
Dave Abrahams <dabrahams@google.com>
2020-04-14 11:24:10 -0700

    Make exampleBatcher run as a test.

--- examples/exampleBatcher.swift
+++ examples/exampleBatcher.swift
@@ -1,20 +1,26 @@
 import TensorFlow
 import Batcher
+import XCTest
 
+class BatcherTests : XCTestCase {
 // Base use
+    func test0() {
 // Some raw items (for instance filenames)
 let rawItems = 0..<512
 // A heavy-compute function lazily mapped on it (for instance, opening the images)
 let dataSet = rawItems.lazy.map { _ in Tensor<Float>(randomNormal: [224, 224, 3]) }
+
 // A `Batcher` defined on this:
 let batcher = Batcher(on: dataSet, batchSize: 64)
 // Iteration over it (for instance, on epoch of the training loop) 
 for batch in batcher.sequenced() {
     print(batch.shape)
 }
+}
 
 // Use with padding
 // Let's create an array of things of various lengths (for instance texts)
+let dataSet: [Tensor<Int32>] = { 
 var dataSet: [Tensor<Int32>] = []
 for _ in 0..<512 {
     dataSet.append(Tensor<Int32>(
@@ -23,7 +29,9 @@ for _ in 0..<512 {
         upperBound: Tensor<Int32>(100)
     ))
 }
-
+return dataSet
+}()
+    
 // We need to pad those tensors to make them all the same length.
 // We could do this in one lazy transform applied beforehand and pad everything
 // to the same length, but it's not memory-efficient: some batches might need less
@@ -37,10 +45,12 @@ func padTensors(tensors: [Tensor<Int32>]) -> [Tensor<Int32>] {
     }
 } 
 
+    func test1() {
 let batcher = Batcher(on: dataSet, batchSize: 64, padSamples: padTensors)
 for b in batcher.sequenced() {
     print(b.shape)
 }
+    }
 
 // Use with a sampler
 // In our previous example, another way to be memory efficient is to batch
@@ -51,10 +61,16 @@ func sortSamples(on dataset: inout [Tensor<Int32>], shuffled: Bool) -> [Int] {
     return Array(0..<dataset.count).sorted { dataset[$0].shape[0] > dataset[$1].shape[0] }
 }
 
+    func test2() {
+
+// Use with a sampler
+// In our previous example, another way to be memory efficient is to batch
+// samples of roughly the same lengths.
 let batches = Batcher(on: dataSet, batchSize: 64, sampleIndices: sortSamples, padSamples: padTensors)
 for b in batches.sequenced() {
     print(b.shape)
 }
+    }
 
 // Sometimes the shuffle method needs to be applied on the dataset itself, 
 // like for language modeling. Here is a base version of that to test
@@ -91,8 +107,10 @@ struct DataSet: RandomAccessCollection {
 }
 
 //Let's create such a DataSet
-let numbers: [[Int]] = [[1,2,3,4,5], [6,7,8], [9,10,11,12,13,14,15], [16,17,18]]
-let dataset = DataSet(numbers: numbers, sequenceLength: 3)
+let dataset: DataSet = {
+    let numbers: [[Int]] = [[1,2,3,4,5], [6,7,8], [9,10,11,12,13,14,15], [16,17,18]]
+    return DataSet(numbers: numbers, sequenceLength: 3)
+}()
 
 // This is the sampler we will use: it always returns the default indices
 // and shuffles the dataset if needed
@@ -102,12 +120,18 @@ func internalShuffle(on dataset: inout DataSet, shuffled: Bool) -> [Int] {
 }
 
 //Now let's look at what it gives us:
+func test3() {
 let batcher = Batcher(on: dataset, batchSize: 3, sampleIndices: internalShuffle)
 for b in batcher.sequenced() {
     print(b)
 }
+    }
+
+    func test4() {
 
 let batcher = Batcher(on: dataset, batchSize: 3, shuffle: true, sampleIndices: internalShuffle)
 for b in batcher.sequenced() {
     print(b)
 }
+
+    }}
