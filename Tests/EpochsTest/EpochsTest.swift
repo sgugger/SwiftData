9e8af395e31cac7905c20f5e3ef5b8a2736cf0b8
Dave Abrahams <dabrahams@google.com>
2020-04-15 13:31:19 -0700

    Fail as executable.
    // Using command:
    TOOLCHAINS=com.google.swift.20200407 swiftc -g -Onone Tests/EpochsTest/EpochsTest.swift Tests/EpochsTest/main.swift Epochs/*.swift -o /tmp/x && export DYLD_LIBRARY_PATH=/Library/Developer/Toolchains/swift-tensorflow-DEVELOPMENT-2020-04-07-a.xctoolchain/usr/lib/swift/macosx/ && /tmp/x

--- Tests/EpochsTest/EpochsTest.swift
+++ Tests/EpochsTest/EpochsTest.swift
@@ -1,14 +1,38 @@
 import TensorFlow
-import Epochs
-import XCTest
-import PcgRandom
+//import Epochs
+//import XCTest
 
-var pcg = Pcg64Random(seed: 42)
+func XCTAssert(
+  _ expression: @autoclosure () -> Bool,
+  _ message: @autoclosure () -> String = "",
+  file: StaticString = #file, line: UInt = #line
+) {
+  assert(expression(), message(), file: file, line: line)
+}
+
+func XCTAssertFalse(
+  _ expression: @autoclosure () -> Bool,
+  _ message: @autoclosure () -> String = "",
+  file: StaticString = #file, line: UInt = #line
+) {
+  assert(!expression(), message(), file: file, line: line)
+}
+
+func XCTAssertEqual<T: Equatable>(
+  _ expression: @autoclosure () -> T,
+  _ expression1: @autoclosure () -> T,
+  _ message: @autoclosure () -> String = "",
+  file: StaticString = #file, line: UInt = #line
+) {
+  assert(expression() == expression1(), message(), file: file, line: line)
+}
+
+var pcg = LinearCongruential(seed: 42)
 let tfSeed: TensorFlowSeed = (
   graph: Int32.random(in:Int32.min..<Int32.max, using: &pcg), 
   op: Int32.random(in:Int32.min..<Int32.max, using: &pcg))
 
-final class EpochsTests : XCTestCase {
+final class EpochsTests /*: XCTestCase*/ {
   // Some raw items (for instance filenames)
   let rawItems: [Int] = Array(0..<512)
   
@@ -242,3 +266,4 @@ extension EpochsTests {
     ("testLanguageModelShuffled", testLanguageModelShuffled),
   ]
 }
+
